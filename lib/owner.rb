962ce1a3e0c9664305e64c08e75d78f8ad4692e7
norio c <norionorioc@gmail.com>
2020-02-26 03:26:20 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -60,7 +60,7 @@ class Owner
   end
   
   def list_pets
-      "I have #{self.dogs.count} dogs(s), and #{self.cats.count} cat(s)."
+      "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
       end
   end
 
